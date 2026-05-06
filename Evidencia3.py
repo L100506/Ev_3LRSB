719f54ee605775dd9377543fa2284661810417a1
Linkdone <logan79081@gmail.com>
2026-05-05 19:47:38 -0600

    Se agrego el Init de la clase

--- Evidencia3.py
+++ Evidencia3.py
@@ -1,1 +1,9 @@
-print('Hola mundo')
+class CryptoMonedas:
+    def __init__(self, nombre, simbolo, precio, volumen, capitalizacion, supply):
+        self.nombre = nombre
+        self.simbolo = simbolo
+        self.__precio = precio
+        self.__volumen = volumen
+        self.__capitalizacion = capitalizacion
+        self.__supply = supply
+
